afee75e25ed623a5bc9f53e520d6c04bd0d0a38c
only1thor <>
2023-12-08 09:17:27 +0100

    reorganize where generated stls are stored

--- generate_letter_pucks.sh
+++ generate_letter_pucks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in {A..L}
+for i in {A..Z}
 do
 	echo "ID=\"${i}\""
-	~/./Downloads/OpenSCAD-2021.01-x86_64.AppImage -D "ID=\"${i}\"" -o "puck_${i}.stl" puck.scad
+	~/./Downloads/OpenSCAD-2021.01-x86_64.AppImage -D "ID=\"${i}\"" -o "3d_files/puck_${i}.stl" puck.scad
 done
